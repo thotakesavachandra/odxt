4ca2e005df7d9a317b0af3d6ccf9adc6293fece2
thotakesavachandra <thotakesavachandra@gmail.com>
2025-02-20 23:23:22 +0530

    resources(sample DBs) addition

---
+++ database/converter.cpp
@@ -0,0 +1,49 @@
+#include "../utils.h"
+
+#ifdef SHUFFLE
+    bool toShuffle = true;
+#endif
+#ifndef SHUFFLE
+    bool toShuffle = false;
+#endif
+
+
+/*
+    Reads the file identifiers from the content
+    stores them in the form of pairs <idx, file_id
+*/
+vector<vector<string>> strip(const vector<vector<string>>& content){
+    vector<vector<string>> ans;
+    for(int i=0; i<content.size(); i++){
+        for(auto x:content[i]){
+            ans.push_back({intToStr(i), intToStr(strToInt(x))});
+        }
+    }
+    return ans;
+}
+
+
+int main(){
+    // reads from the db6k.dat file
+
+    auto content = read_file("db6k.dat");
+    
+    // erase the keyword part
+    for(auto& row:content){
+        row.erase(row.begin());
+    }
+
+    if(toShuffle){
+        shuffle(content.begin(), content.end(), default_random_engine(0));
+    }
+
+    auto updates = strip(content);
+
+    // lets shuffle the updates
+    shuffle(updates.begin(), updates.end(), default_random_engine(0));
+
+    write_file("db6k_updates.csv", updates);
+
+
+    return 0;
+}
